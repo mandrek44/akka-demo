ca23ca7ee3d876a8d0a5de0deb95f18236cdf785
Marcin Drobik <mdrobik@future-processing.com>
2015-09-21 21:59:50 +0200

    13

--- Actors.cs
+++ Actors.cs
@@ -1,8 +1,14 @@
-using Akka.Actor;
+﻿using Akka.Actor;
 
 namespace FDD.Akka
 {
     class AttachmentScannerActor : ReceiveActor
     {
+        public AttachmentScannerActor()
+        {
+            Receive<ScanAttachment>(message =>
+            {
+            });
+        }
     }
 }
